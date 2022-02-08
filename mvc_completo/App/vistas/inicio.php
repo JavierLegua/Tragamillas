3f4aea4cb78f01079cbef6bb722eb3ec1df73329
Raul <raul.dkl.37@gmail.com>
2022-02-08 14:16:00 +0100

    comentario sobre la ruta de css

--- mvc_completo/App/vistas/inicio.php
+++ mvc_completo/App/vistas/inicio.php
@@ -7,7 +7,7 @@
     <title>Pagina de inicio</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css"> <!-- cambiar la ruta del css, esta mal -->
 </head>
 <?php require_once RUTA_APP.'/vistas/inc/header.php' ?>
 <?php if (tienePrivilegios($datos['usuarioSesion']->idRol,[1,2])):?>
