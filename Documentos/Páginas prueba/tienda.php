2794cb1f963df7aebf252ad587b30335242b68be
Raul <raul.dkl.37@gmail.com>
2022-02-09 10:33:25 +0100

    archivos + login empezado

---
+++ Documentos/Páginas prueba/tienda.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section>
+        <div class="container">
+            <div class="row d-flex justify-content-around" style="margin-bottom: 50px;">
+                
+                <div class="borde border col-lg-3 col-md-3 p-0 tarjeta">
+                    <div class="card-header d-flex justify-content-center colortarjeta">
+                        <i class="bi bi-person-badge iconsize"></i>
+                    </div>
+
+                    <div class="card-body">
+                        
+                        <p class="card-text">
+                            <div class="accordion accordion-flush" id="accordionFlushExample1">
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="flush-headingOne1">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne1" aria-expanded="false" aria-controls="flush-collapseOne1">
+                                        <h5 class="card-title text-center">Socios</h5>
+                                        </button>
+                                    </h2>
+                                    <div id="flush-collapseOne1" class="accordion-collapse collapse" aria-labelledby="flush-headingOne1" data-bs-parent="#accordionFlushExample1">
+                                        <div class="accordion-body">
+                                            <a href="#">Consultar Socios</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </p>
+                    </div>
+                </div>
+
+                <div class="borde border col-lg-3 col-md-3 p-0 tarjeta">
+                    <div class="card-header d-flex justify-content-center colortarjeta">
+                        <i class="bi bi-journal-check iconsize"></i>
+                    </div>
+
+                    <div class="card-body">
+                        
+                        <p class="card-text">
+                            <div class="accordion accordion-flush" id="accordionFlushExample3">
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="flush-headingOne3">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                            <h5 class="card-title text-center">Reparto Ropa </h5>
+                                        </button>
+                                    </h2>
+                                    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne3" data-bs-parent="#accordionFlushExample3">
+                                        <div class="accordion-body">
+                                            <a href="#">Ropa Recogida</a> <br>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </p>
+                    </div>
+                </div>
+            
+            </div>
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
