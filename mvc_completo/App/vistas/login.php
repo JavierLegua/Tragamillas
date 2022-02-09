5e311654f67abb1ac0ddfde9aa25e4e13010e704
Raul <raul.dkl.37@gmail.com>
2022-02-09 11:11:04 +0100

    .

--- mvc_completo/App/vistas/login.php
+++ mvc_completo/App/vistas/login.php
@@ -11,21 +11,21 @@
     <link rel="stylesheet" href="<?php echo RUTA_URL?>/css/estilos.css">
 </head>
 
-<body class="bg-primary">
+<body>
 <section class="vh-100 gradient-custom">
 
   <div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100 ">
       <div class="col-12 col-md-8 col-lg-6 col-xl-5">
 
-        <div class="card text-white justify-content-center align-items-center" style="border-radius: 1rem;" >
+        <div class="card text-white justify-content-center align-items-center shadow p-3 mb-5 bg-body rounded" style="border-radius: 1rem;" >
         
             <img src="<?php echo RUTA_URL?>/public/img/logo_tragamillas.png" alt="" width="291" height="217" class="avatar">
         
        
           <div class="card-body p-5 text-center">
 
-            <div class="mb-md-5 mt-md-5 pb-5">
+            <div class="mb-md-5 mt-md-5 pb-5 ">
               <form action="datos_login.php" method="post">
                 <div class="form-outline form-white mb-4">
                   <input type="email" name="email" id="email" class="form-control form-control-lg" placeholder="Usuario (email)" required/>
