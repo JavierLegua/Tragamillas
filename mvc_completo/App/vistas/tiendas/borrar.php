42909df6420f873689f1450448e47c5545b387e5
Raul <raul.dkl.37@gmail.com>
2022-02-14 13:19:01 +0100

    borrar

--- mvc_completo/App/vistas/tiendas/borrar.php
+++ mvc_completo/App/vistas/tiendas/borrar.php
@@ -8,15 +8,15 @@
     <form method="post" class="card-body">
         <div class="mt-3 mb-3">
             <label for="nombre">Nombre: <sup>*</sup></label>
-            <input type="text" name="nombre" id="nombre" class="form-control form-control-lg" value="<?php echo $datos['usuario']->apellidoUsuario ?>" disabled>
+            <input type="text" name="nombre" id="nombre" class="form-control form-control-lg" value="<?php echo $datos['tienda']->apellidoUsuario ?>" disabled>
         </div>
         <div class="mb-3">
             <label for="email">Email: <sup>*</sup></label>
-            <input type="email" name="email" id="email" class="form-control form-control-lg" value="<?php echo $datos['usuario']->email ?>" disabled>
+            <input type="email" name="email" id="email" class="form-control form-control-lg" value="<?php echo $datos['tienda']->email ?>" disabled>
         </div>
         <div class="mb-3">
             <label for="telefono">Teléfono: <sup>*</sup></label>
-            <input type="text" name="telefono" id="telefono" class="form-control form-control-lg" value="<?php echo $datos['usuario']->telefono ?>" disabled>
+            <input type="text" name="telefono" id="telefono" class="form-control form-control-lg" value="<?php echo $datos['tienda']->telefono ?>" disabled>
         </div>
         <input type="submit" class="btn btn-success" value="Borrar Usuario">
     </form>
