79e396b44ee4a8bbde1c6f80fb36aa436b9483a2
Raul <raul.dkl.37@gmail.com>
2022-02-14 12:56:53 +0100

    arreglo footer con el <script>

--- mvc_completo/App/vistas/inc/footer.php
+++ mvc_completo/App/vistas/inc/footer.php
@@ -1,8 +1,4 @@
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script src="<?php echo RUTA_URL?>/js/main.js"></script>
-    <link rel="stylesheet" href="<?php echo RUTA_URL?>/css/estilos.css">
-</body>
 <footer class="footer">
     <div class="container-fluid position-absolute bottom-0 end-0 color">
         <div class="row" style="height: 70px;">
@@ -28,4 +24,7 @@
         </div>
     </div>
 </footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="<?php echo RUTA_URL?>/js/main.js"></script>
+</body>
 </html>
