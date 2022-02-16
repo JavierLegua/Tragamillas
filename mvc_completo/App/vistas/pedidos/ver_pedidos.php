d644bd480db3f2ae604a6682ab30c22ca868f3bd
Faced <angelfaced@hotmail.es>
2022-02-16 12:00:03 +0100

    crud pedidos terminado

--- mvc_completo/App/vistas/pedidos/ver_pedidos.php
+++ mvc_completo/App/vistas/pedidos/ver_pedidos.php
@@ -13,24 +13,19 @@
         </thead>
         <tbody>
             <?php foreach($datos['pedido'] as $uruario): ?>
-                <tr>
-                    <td><?php echo $uruario->idEquipacion ?></td>
-                    <td><?php echo $uruario->talla ?></td>
-                    <td><?php echo $uruario->idUsuario ?></td>
-                    <td><?php echo $uruario->apellidoUsuario ?></td>
-<?php if (tienePrivilegios($datos['usuarioSesion']->idRol,[4]) && $uruario->entregado != 1):?>
-                    <td>
-                        <a href="<?php echo RUTA_URL?>/pedidos/confirmarPedido/<?php echo $uruario->idEquipacion ?>">Confirmar entrega</a>
-                        &nbsp;&nbsp;&nbsp;&nbsp;
-                        <a href="<?php echo RUTA_URL?>/pedidos/borrarPedido/<?php echo $uruario->idEquipacion ?>">Borrar pedido</a>
-                    </td>
-<?php endif ?>
-<?php if (tienePrivilegios($datos['usuarioSesion']->idRol,[4]) && $uruario->entregado == 1):?>
-                    <td>
-                        <?php echo 'pedido entregado'?>
-                    </td>
-<?php endif ?>
-                </tr>
+                <?php if (tienePrivilegios($datos['usuarioSesion']->idRol,[4]) && $uruario->entregado == !1):?>
+                    <tr>
+                        <td><?php echo $uruario->idEquipacion ?></td>
+                        <td><?php echo $uruario->talla ?></td>
+                        <td><?php echo $uruario->idUsuario ?></td>
+                        <td><?php echo $uruario->apellidoUsuario ?></td>
+                        <td>
+                            <a href="<?php echo RUTA_URL?>/pedidos/confirmarPedido/<?php echo $uruario->idEquipacion ?>">Confirmar entrega</a>
+                            &nbsp;&nbsp;&nbsp;&nbsp;
+                            <a href="<?php echo RUTA_URL?>/pedidos/borrarPedido/<?php echo $uruario->idEquipacion ?>">Borrar pedido</a>
+                        </td>                
+                    </tr>
+                <?php endif ?>
             <?php endforeach ?>
         </tbody>
     </table>
