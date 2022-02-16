c34dbe1c70676550a32f26161b91cbff6e33386b
Faced <angelfaced@hotmail.es>
2022-02-16 13:28:16 +0100

    Ay por la raza que se ma matao mijo

--- mvc_completo/App/vistas/inc/header.php
+++ mvc_completo/App/vistas/inc/header.php
@@ -24,7 +24,7 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav me-auto mb-2 mb-md-0">
-<?php if (tienePrivilegios($datos['usuarioSesion']->idRol,[1,2])):?>
+<?php if (tienePrivilegios($datos['usuarioSesion']->idRol,[1])):?>
                     <li class="nav-item">
                         <?php if (isset($datos['menuActivo']) && $datos['menuActivo'] == 1 ): ?>
                             <a class="nav-link active" aria-current="page" href="<?php echo RUTA_URL ?>/usuarios">Usuarios</a>
