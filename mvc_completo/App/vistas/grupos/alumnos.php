0c148ce5540fdfbe73d23cf658bb087521c32c53
Javier Legua <javierlegua14@gmail.com>
2022-03-10 12:29:45 +0000

    Avances

--- mvc_completo/App/vistas/grupos/alumnos.php
+++ mvc_completo/App/vistas/grupos/alumnos.php
@@ -1,21 +1,25 @@
 <?php require_once RUTA_APP.'/vistas/inc/header.php' ?>
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Id alumno</th>
-                <th>Nombre</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($datos['grupo'] as $uruario): ?>
+<main class="flex-shrink-0 margenTop">
+    <div class="container">
+        <table class="table">
+            <thead>
                 <tr>
-                    <td><?php echo $uruario->idUsuario ?></td>
-                    <td><?php echo $uruario->apellidoUsuario ?></td>
+                    <th>Id alumno</th>
+                    <th>Nombre</th>
                 </tr>
-            <?php endforeach ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach($datos['grupo'] as $uruario): ?>
+                    <tr>
+                        <td><?php echo $uruario->idUsuario ?></td>
+                        <td><?php echo $uruario->apellidoUsuario ?></td>
+                    </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
 
-    <a class="btn btn-primary justify-content-center" href="<?php echo RUTA_URL?>/grupos/cerrarGrupos/<?php echo $this->datos['idGrupo']?>">Cerrar grupo</a>
-    <a class="btn btn-primary justify-content-center" href="<?php echo RUTA_URL?>/grupos/abrirGrupos/<?php echo $this->datos['idGrupo']?>">Abrir grupo</a>
+        <a class="btn btn-primary justify-content-center" href="<?php echo RUTA_URL?>/grupos/cerrarGrupos/<?php echo $this->datos['idGrupo']?>">Cerrar grupo</a>
+        <a class="btn btn-primary justify-content-center" href="<?php echo RUTA_URL?>/grupos/abrirGrupos/<?php echo $this->datos['idGrupo']?>">Abrir grupo</a>
+    </div>    
+</main>
 <?php require_once RUTA_APP.'/vistas/inc/footer.php' ?>
